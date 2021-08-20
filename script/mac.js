9e5c509c20df13ec47092fd96de384c2f3a50bbb
rakibshakib <rakibshakib74@gmail.com>
2021-08-21 00:48:35 +0600

    funtionality complete: complete all js function in this project

--- script/mac.js
+++ script/mac.js
@@ -1,1 +1,58 @@
-console.log("script connected");
+const finalPrice = document.getElementById('final-total-price');
+
+function itemsPrice(item, price) {
+    const itemPrice = document.getElementById(item + '-price');
+    const companyMemoryPrice = 0 + price;
+    itemPrice.innerText = companyMemoryPrice;
+    totalPrice();
+}
+
+function totalPrice() {
+    const regularPrice = document.getElementById('regular-price').innerText;
+    const memoryPrice = document.getElementById('memory-price').innerText;
+    const storagePrice = document.getElementById('storage-price').innerText;
+    const deliveryPrice = document.getElementById('delivery-price').innerText;
+    const itemTotalPrice = parseInt(regularPrice) + parseInt(memoryPrice) + parseInt(storagePrice) + parseInt(deliveryPrice);
+    const totalPrice = document.getElementById('total-price');
+    totalPrice.innerText = itemTotalPrice;
+    finalPrice.innerText = itemTotalPrice;
+    return itemTotalPrice;
+}
+
+// couple calculation
+function discount() {
+    const promoInput = document.getElementById('promo-input');
+    const itemTotalPrice = totalPrice();
+    let discountPrice;
+    if (promoInput.value == 'stevekaku') {
+        discountPrice = itemTotalPrice * (20 / 100);
+        promoInput.value = '';
+    }
+    const afterDiscountTotalPrice = itemTotalPrice - discountPrice;
+    finalPrice.innerText = parseInt(afterDiscountTotalPrice);
+}
+
+document.getElementById('normal-memory').addEventListener('click', function () {
+    itemsPrice('memory', 0)
+})
+document.getElementById('extra-memory').addEventListener('click', function () {
+    itemsPrice('memory', 180)
+})
+document.getElementById('normal-ssd').addEventListener('click', function () {
+    itemsPrice('storage', 0);
+})
+document.getElementById('fifth-gb-ssd').addEventListener('click', function () {
+    itemsPrice('storage', 100);
+})
+document.getElementById('one-tera-ssd').addEventListener('click', function () {
+    itemsPrice('storage', 180);
+})
+document.getElementById('normal-delivery').addEventListener('click', function () {
+    itemsPrice('delivery', 0);
+})
+document.getElementById('fast-delivery').addEventListener('click', function () {
+    itemsPrice('delivery', 20);
+})
+document.getElementById('promo-btn').addEventListener('click', function () {
+    discount();
+})
